83ba3217db17601787fc0edfd7c01a33ae84853f
davidelolli99 <davidelolli58@gmail.com>
2026-08-23 12:19:25 +0200

    sss

--- app.js
+++ app.js
@@ -1,4 +1,18 @@
-function saveExpense() {
+const SUPABASE_URL =
+    "https://yumndwfqjxkboaputyia.supabase.co";
+
+const SUPABASE_KEY =
+    "sb_publishable_9dhDmCkwaK9a5LkRk-jCPQ_o7Dxn0bW";
+
+const { createClient } = supabase;
+
+const supabaseClient =
+    createClient(
+        SUPABASE_URL,
+        SUPABASE_KEY
+    );
+
+async function saveExpense() {
 
     const date =
         document.getElementById("date").value;
@@ -9,9 +23,40 @@ function saveExpense() {
     const category =
         document.getElementById("category").value;
 
-    alert(
-        "Data: " + date +
-        "\nImporto: " + amount +
-        "\nCategoria: " + category
-    );
+    if (!date || !amount) {
+        alert("Inserisci data e importo");
+        return;
+    }
+
+    try {
+
+        const response =
+            await supabaseClient
+                .from("expenses")
+                .insert([
+                    {
+                        expense_date: date,
+                        amount: Number(amount),
+                        category: category
+                    }
+                ]);
+
+        console.log(response);
+
+        if (response.error) {
+            alert("Errore: " + response.error.message);
+            return;
+        }
+
+        alert("Spesa salvata!");
+
+    } catch (error) {
+
+        console.error(error);
+
+        alert(
+            "Errore: " +
+            error.message
+        );
+    }
 }
