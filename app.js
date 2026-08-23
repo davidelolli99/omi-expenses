8e04a3e7a437f6d6eb8145052f6e013b22fa6e2a
davidelolli99 <davidelolli58@gmail.com>
2026-08-23 12:13:28 +0200

    test

--- app.js
+++ app.js
@@ -1,4 +1,16 @@
-alert("APP JS CARICATO");
+const SUPABASE_URL =
+  "https://yumndwfqjxkboaputyia.supabase.co";
+
+const SUPABASE_KEY =
+  "LA_TUA_PUBLISHABLE_KEY";
+
+const supabaseClient =
+  supabase.createClient(
+    SUPABASE_URL,
+    SUPABASE_KEY
+  );
+
 function saveExpense() {
-    alert("SALVA PREMUTO");
+    alert("Entrato nella funzione");
+    console.log("Supabase:", supabaseClient);
 }
